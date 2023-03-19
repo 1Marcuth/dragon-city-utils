dd1461585d75d0a84c61c333b28538dd76cd6abd
TantoFaz <thiago.playgames15@gmail.com>
2023-03-19 17:17:50 -0300

    1.17

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as file:
     readme_content = file.read()
 
+with open(
+    "requirements.txt",
+    mode="r",
+    encoding="UTF-16"
+) as file:
+    requirements = [ line.split("==")[0].strip() for line in file.readlines() ]
+
 setup(
     name="dragon-city-utils",
-    version="1.16",
+    version="1.17",
     license="MIT License",
     author="Marcuth",
     long_description=readme_content,
@@ -14,5 +21,5 @@ setup(
     keywords="dragoncity dcutils tools",
     description=f"Utilities and tools for things related to Dragon City",
     packages=["dcutils"] + [ "dcutils/" + x for x in find_packages("dcutils") ],
-    install_requires=["httpx", "pydantic", "python-filter"]
+    install_requires=requirements
 )
