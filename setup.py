e185e22c9da404b40fdb8f0cb7eef5e885219294
TantoFaz <thiago.playgames15@gmail.com>
2023-04-23 22:27:03 -0300

    Imprementação de IA e url parser

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as file:
     readme_content = file.read()
 
-with open(
-    "requirements.txt",
-    mode="r",
-    encoding="UTF-16"
-) as file:
-    requirements = [ line.split("==")[0].strip() for line in file.readlines() ]
+# with open(
+#     "requirements.txt",
+#     mode="r",
+#     encoding="UTF-16"
+# ) as file:
+#     requirements = [ line.split("==")[0].strip() for line in file.readlines() ]
+
+requirements = ['anyio', 'bleach', 'certifi', 'charset-normalizer', 'docutils', 'h11', 'httpcore', 'httpx', 'idna', 'importlib-metadata', 'jaraco.classes', 'keyring', 'markdown-it-py', 'mdurl', 'more-itertools', 'pkginfo', 'pydantic', 'Pygments', 'python-filter', 'pywin32-ctypes', 'readme-renderer', 'requests', 'requests-toolbelt', 'rfc3986', 'rich', 'six', 'sniffio', 'twine', 'typing_extensions', 'urllib3', 'webencodings', 'zipp']
 
 setup(
     name="dragon-city-utils",
-    version="1.17",
+    version="1.17.3.8",
     license="MIT License",
     author="Marcuth",
     long_description=readme_content,
